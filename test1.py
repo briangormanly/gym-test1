e19380d5194a4e55f721dc75de743aed24bf964f
Brian Gormanly <brian.gormanly@marist.edu>
2016-05-05 09:30:53 -0400

    added a mean timestamp to better measure improvements

--- test1.py
+++ test1.py
@@ -1,5 +1,5 @@
 import gym
-
+import numpy
 
 # What gets called
 def act(observation, reward, done):
@@ -21,6 +21,10 @@ def act(observation, reward, done):
     return action
 
 env = gym.make('CartPole-v0')
+
+# store results to average
+timestamps = [];
+
 #env.monitor.start('tmp/cartpole-experiment-1')
 for i_episode in xrange(20):
     observation = env.reset()
@@ -33,7 +37,9 @@ for i_episode in xrange(20):
         action = act(observation, reward, done)
         observation, reward, done, info = env.step(action)
         if done:
+            timestamps.append(t+1) 
             print "Episode finished after {} timesteps ".format(t+1)
+            print "Episode mean timestamp was: {}".format(numpy.mean(timestamps))
             break
 
 #env.monitor.close()
